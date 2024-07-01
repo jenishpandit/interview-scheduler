078c73af0f7f2724d07b9f747d90c56007f40801
chintan-harkhani33 <chintan.harkhani@ghanshyamdigital.com>
2024-07-01 19:26:27 +0530

    dashbord-page-chnage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,39 +1,79 @@
 "use client";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import React, { useEffect, useState } from "react";
 import { Cpu, Laptop, User } from "lucide-react";
 import axios from "../lib/axios";
-import DashbordCard from "@/components/DashbordCard";
-import TodayInterview from "@/components/todayinterview";
+import DashbordCard from "@/components/dashbordCard";
+import TodayInterview from "@/components/interview";
+import moment from "moment";
 
 type DashboardData = {
   candidateTotal: number;
   technologyTotal: number;
   interviewTotal: number;
 };
+interface IInterview {
+  id: string;
+  interview_date: Date;
+  interview_type: string;
+  location: string;
+}
+interface Technology {
+  _id: string;
+  name: string;
+}
 const page = () => {
   const [dashboard, SetDashboard] = useState<DashboardData | null>(null);
+  const [getinterview, setInterview] = useState<IInterview[]>([]);
+  const [Gettecnology, setTechnologies] = useState<Technology[]>([]);
+  const [selectedTab, setSelectedTab] = useState("today");
 
   const fetchDashboard = async () => {
     try {
       const res = await axios.get<{ data: DashboardData }>("/dashboard/total");
-      console.log("Fetched data:", res.data);
+      // console.log("Fetched data:", res.data);
       SetDashboard(res.data.data);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
-
   useEffect(() => {
     fetchDashboard();
   }, []);
+  const InterviewFetch = async (tab:any) => {
+    try {
+      let endpoint:string;
+      // if (tab === "Today") {
+      //   endpoint = "/interview?date=today";
+      // }
+      const res = await axios.get(`/interview?filter=${tab}`);
+      
+      setInterview(res.data.data);
+    } catch (error) {
+      console.error('Error fetching interviews:', error);
+    }
+  };
+  useEffect(() => {
+    InterviewFetch(selectedTab);
+  }, [selectedTab]);
 
+  const fetchTechnology = async () => {
+    try {
+      const res = await axios.get("/technology");
+      // console.log("Fetched data tech:", res.data.data);
+      setTechnologies(res.data.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchTechnology();
+  }, []);
+
+  useEffect(() => {// Update filtered interviews when tab changes
+  }, [selectedTab, getinterview]);
+
+console.log(getinterview);
+  //  console.log("=======",getinterview)
   return (
     <>
       <div className="flex justify-between items-center p-4 pt-6 border-b border-gray-300">
@@ -43,7 +83,7 @@ const page = () => {
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 ">
           <DashbordCard
             title="Candidate Total"
-            value ={ dashboard?.candidateTotal}
+            value={dashboard?.candidateTotal}
             Icon={<User />}
           ></DashbordCard>
           <DashbordCard
@@ -58,8 +98,14 @@ const page = () => {
           ></DashbordCard>
         </div>
       </div>
-
-      <TodayInterview></TodayInterview>
+      <div className="Today_interview ">
+        <TodayInterview
+         getInterview={getinterview}
+          Gettecnology={Gettecnology}
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        />
+      </div>
     </>
   );
 };
