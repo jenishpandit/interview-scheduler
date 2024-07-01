af2bb80f579c177d6f32116197a5e80686a32635
chintan-harkhani33 <chintan.harkhani@ghanshyamdigital.com>
2024-07-01 19:28:49 +0530

    change dashborad

--- src/components/interview.tsx
+++ src/components/interview.tsx
@@ -158,17 +158,6 @@ const TodayInterview: React.FC<TodayInterviewProps> = ({Gettecnology ,selectedTa
             today
           </TabsTrigger>
           <TabsTrigger
-            value="tomorrow"
-            className={`inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ${
-              selectedTab === "tomorrow"
-                ? "bg-background text-foreground shadow-sm"
-                : "disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm"
-            }`}
-            onClick={() => setSelectedTab("tomorrow")}
-          >
-            Tomorrow
-          </TabsTrigger>
-          <TabsTrigger
           value="upcoming"
           className={`inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ${
             selectedTab === "upcoming"
